4487de1d0c0b72e45378ef83cb72385d889843c4
Ramaneep Singh Bhomrah <88532581+ramandeepbhomrah@users.noreply.github.com>
2024-06-03 23:52:41 +1000

    Update jenkins.py

--- jenkins.py
+++ jenkins.py
@@ -4,7 +4,7 @@ pipeline {
         REPO_URL = "https://github.com/ramandeepbhomrah/Jenkins6.1p.git"
         REPO_PATH = "/path/user/raman"
         DEV_ENV = "development-env"
-        PROD_ENV = "Ramand"
+        PROD_ENV = "Ramandeep"
     }
     stages {
         stage('Clone Repository') {
