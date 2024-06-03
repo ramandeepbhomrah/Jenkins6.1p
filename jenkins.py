7ca960b78992f6fd5913a229f89d9d49f45ac5ae
Ramaneep Singh Bhomrah <88532581+ramandeepbhomrah@users.noreply.github.com>
2024-06-03 21:21:59 +1000

    Update jenkins.py

--- jenkins.py
+++ jenkins.py
@@ -1,15 +1,16 @@
 pipeline {
     agent any
     environment {
+        REPO_URL = "https://github.com/ramandeepbhomrah/Jenkins6.1p.git"
         REPO_PATH = "/path/user/raman"
         DEV_ENV = "development-env"
-        PROD_ENV = "ramandeep"
+        PROD_ENV = "production-env"
     }
     stages {
         stage('Clone Repository') {
             steps {
                 echo "Cloning the repository from the path: ${env.REPO_PATH}"
-                git url: 'https://github.com/your-repo.git', branch: 'main'
+                git url: "${env.REPO_URL}", branch: 'main', credentialsId: '4eba3e14-63ea-4039-8ca7-8f0a45bd4a57'
             }
         }
         stage('Compile') {
@@ -57,7 +58,7 @@ pipeline {
                     emailext(
                         subject: "Build Successful",
                         body: "The build and integration tests have passed. Artifacts are archived.",
-                        to: "rsb132500000@gmail.com"
+                        to: "your.email@example.com"
                     )
                 }
                 failure {
@@ -65,8 +66,7 @@ pipeline {
                     emailext(
                         subject: "Build Failed",
                         body: "The build or integration tests failed. Please check the Jenkins logs.",
-                        to: "rsb132500000@gmail.com"
-
+                        to: "your.email@example.com"
                     )
                 }
             }
